7b19a4f86832ab74e78c230657541da716ebd6ca
hoai.truong <hoaitruong@hoaitruong.local>
2022-03-14 13:04:42 +0800

    get all cleaners

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,42 +1,52 @@
 // @ts-nocheck
-import React, { useState } from "react"
-import { connect } from "react-redux"
-import { Redirect, Route, Switch } from "react-router-dom"
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Link, Redirect, Route, Switch } from "react-router-dom";
 
-import JobList from "./JobList"
-import { Container, Sidebar, Loader, Sidenav, Dropdown, Nav, Content, Navbar } from "rsuite"
-import Header from "rsuite/esm/Calendar/Header"
-import SettingIcon from '@rsuite/icons/Setting';
-import ArrowLeftLineIcon from '@rsuite/icons/ArrowLeftLine';
-import ArrowRightLineIcon from '@rsuite/icons/ArrowRightLine';
+import JobList from "./JobList";
+import CleanerList from "./Cleaner/CleanerList";
+import {
+  Container,
+  Sidebar,
+  Loader,
+  Sidenav,
+  Dropdown,
+  Nav,
+  Content,
+  Navbar,
+} from "rsuite";
+import Header from "rsuite/esm/Calendar/Header";
+import SettingIcon from "@rsuite/icons/Setting";
+import ArrowLeftLineIcon from "@rsuite/icons/ArrowLeftLine";
+import ArrowRightLineIcon from "@rsuite/icons/ArrowRightLine";
 
 interface Props {
   adminRole: {
-    token?: string
-    email?: string
-  }
+    token?: string;
+    email?: string;
+  };
 }
 
 interface State {
-  adminInfo: AdminReducer
+  adminInfo: AdminReducer;
 }
 
 const headerStyles = {
   padding: 18,
   fontSize: 16,
   height: 56,
-  background: '#34c3ff',
-  color: ' #fff',
-  whiteSpace: 'nowrap',
-  overflow: 'hidden'
+  background: "#34c3ff",
+  color: " #fff",
+  whiteSpace: "nowrap",
+  overflow: "hidden",
 };
 
 const iconStyles = {
   width: 56,
   height: 56,
   padding: 18,
-  lineHeight: '56px',
-  textAlign: 'center'
+  lineHeight: "56px",
+  textAlign: "center",
 };
 
 const data = [
@@ -71,25 +81,25 @@ const data = [
       },
     ],
   },
-]
+];
 
 const NavToggle = ({ expand, onChange }) => {
   return (
     <Navbar appearance="subtle" className="nav-toggle">
       <Navbar.Body>
         <Nav>
-          <Dropdown
-            placement="topStart"
-            trigger="click"
-          >
+          <Dropdown placement="topStart" trigger="click">
             <Dropdown.Item>Help</Dropdown.Item>
             <Dropdown.Item>Settings</Dropdown.Item>
             <Dropdown.Item>Sign out</Dropdown.Item>
           </Dropdown>
         </Nav>
         <Nav pullRight>
-          <Nav.Item onClick={onChange} style={{ width: 56, textAlign: 'center' }}>
-            {expand ? <ArrowLeftLineIcon /> : <ArrowRightLineIcon/>}
+          <Nav.Item
+            onClick={onChange}
+            style={{ width: 56, textAlign: "center" }}
+          >
+            {expand ? <ArrowLeftLineIcon /> : <ArrowRightLineIcon />}
           </Nav.Item>
         </Nav>
       </Navbar.Body>
@@ -98,15 +108,16 @@ const NavToggle = ({ expand, onChange }) => {
 };
 
 const Dashboard = (props: Props) => {
-  const [expand, setExpand] = useState(true)
+  const [expand, setExpand] = useState(true);
   if (!props.adminRole?.token) {
-    return <Redirect to="/login" />
+    return <Redirect to="/login" />;
   }
+
   return (
     <div className="show-fake-browser sidebar-page">
       <Container>
         <Sidebar
-          style={{ display: 'flex', flexDirection: 'column' }}
+          style={{ display: "flex", flexDirection: "column" }}
           width={expand ? 260 : 56}
           collapsible
         >
@@ -116,19 +127,25 @@ const Dashboard = (props: Props) => {
               <span style={{ marginLeft: 12 }}> DASHBOARD</span>
             </div>
           </Sidenav.Header>
-          <Sidenav expanded={expand} defaultOpenKeys={['3']} appearance="subtle">
+          <Sidenav
+            expanded={expand}
+            defaultOpenKeys={["3"]}
+            appearance="subtle"
+          >
             <Sidenav.Body>
               <Nav>
-              <Dropdown
+                <Dropdown
                   eventKey="0"
                   trigger="hover"
                   title="Home"
                   placement="rightStart"
                 >
-                  <Dropdown.Item eventKey="2">All Jobs</Dropdown.Item>
-                  </Dropdown>
+                  <Dropdown.Item eventKey="2">
+                    <Link to="/admin/dashboard">All Jobs</Link>
+                  </Dropdown.Item>
+                </Dropdown>
                 <Nav.Item eventKey="12">
-                  Cleaners
+                  <Link to="/admin/cleaners">Cleaners</Link>
                 </Nav.Item>
                 <Dropdown
                   eventKey="13"
@@ -149,28 +166,29 @@ const Dashboard = (props: Props) => {
             <h2>Page Title</h2>
           </Header>
           <Content>
-          <Switch>
-            <Route
-              exact
-              path="/admin"
-              render={() => <Redirect to={`/admin/dashboard`} />}
-            />
-            <Route exact path="/admin/:page" component={JobList} />
-          </Switch>
-          <Loader />
+            <Switch>
+              <Route
+                exact
+                path="/admin"
+                render={() => <Redirect to={`/admin/dashboard`} />}
+              />
+              <Route exact path="/admin/dashboard" component={JobList} />
+              <Route exact path="/admin/cleaners" component={CleanerList} />
+            </Switch>
+            {/* <Loader /> */}
           </Content>
         </Container>
       </Container>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state: State) => {
   return {
     adminRole: state?.adminInfo?.adminAuth,
-  }
-}
+  };
+};
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard as any)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard as any);
--- src/pages/admin/reducer.ts
+++ src/pages/admin/reducer.ts
@@ -1,14 +1,15 @@
 // @ts-nocheck
-import { LOGIN, GET_ALL_JOBS } from "./actions"
-import getJobsList from "./transforms"
+import { LOGIN, GET_ALL_JOBS, GET_ALL_CLEANERS } from "./actions";
+import getJobsList from "./transforms";
 
 export interface AdminReducer {
-  type: string
+  type: string;
   adminAuth?: {
-    token?: string
-    email?: string
-  }
-  jobs: Object
+    token?: string;
+    email?: string;
+  };
+  jobs: Object;
+  cleaners: Object;
 }
 
 const initState = {
@@ -18,30 +19,40 @@ const initState = {
     email: "",
   },
   jobs: {},
-}
+  cleaners: {},
+};
 
 function adminReducer(state: AdminReducer = initState, action: any) {
   switch (action.type) {
     case LOGIN.SUCCEED:
       console.log({
         adminData: action.data,
-      })
+      });
       return {
         ...state,
         adminAuth: action.data,
-      }
+      };
     case GET_ALL_JOBS.SUCCEED:
       return {
         ...state,
         jobs: {
           list: getJobsList(action.data.jobs),
           hasMore: action.data.hasMore,
-          offset: action.data.offset
+          offset: action.data.offset,
         },
-      }
+      };
+    case GET_ALL_CLEANERS.SUCCEED:
+      return {
+        ...state,
+        cleaners: {
+          list: action.data.cleaners,
+          hasMore: action.data.hasMore, 
+          offset: action.data.offset,
+        }
+      };
     default:
-      return state
+      return state;
   }
 }
 
-export default adminReducer
+export default adminReducer;
