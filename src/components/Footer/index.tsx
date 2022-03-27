b36930ea8264c4b4a2f60385587c8b759e5d35ca
hue kim <kimhuebk94@gmail.com>
2022-03-27 21:53:21 +0700

    Add facebook zalo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
-import React, { FC, useEffect } from "react"
-import styled from "styled-components"
+import React, { FC, useEffect } from "react";
+import styled from "styled-components";
+import Chatboxes from "./../Chatboxes";
 
 const Left = styled.div`
   text-align: left;
@@ -9,7 +10,7 @@ const Left = styled.div`
   @media (min-width: 768px) {
     width: 28%;
   }
-`
+`;
 
 const Center = styled.div`
   text-align: left;
@@ -21,7 +22,7 @@ const Center = styled.div`
     margin: 0 30px;
     margin-top: 0;
   }
-`
+`;
 
 const Right = styled.div`
   text-align: left;
@@ -36,29 +37,29 @@ const Right = styled.div`
   @media (min-width: 1024px) {
     width: 27%;
   }
-`
+`;
 
 const Wrapper = styled.footer`
   padding: 20px;
   margin: auto;
-  background-color: #042C41;
+  background-color: #042c41;
   @media (min-width: 640px) {
     justify-content: space-between;
     padding: 51px;
   }
-`
+`;
 
 const Name = styled.h3`
   font-size: 20px;
   color: white;
-`
+`;
 
 const FBPage = styled.a`
   cursor: pointer;
   font-size: 20px;
   word-break: break-all;
   width: 100%;
-`
+`;
 
 const ContactWrapper = styled.ul`
   list-style: none;
@@ -69,7 +70,7 @@ const ContactWrapper = styled.ul`
     justify-content: space-between;
     margin-top: 0px;
   }
-`
+`;
 
 const ContactItem = styled.li`
   font-size: 15px;
@@ -79,37 +80,37 @@ const ContactItem = styled.li`
   display: flex;
   align-items: baseline;
   font-weight: 300;
-`
+`;
 
 const ContactText = styled.span`
   margin-left: 5px;
-`
+`;
 
 const Contact = styled.strong`
   color: white;
   font-size: 18px;
   display: block;
   padding-top: 10px;
-`
+`;
 
 const NetworkIcons = styled.div`
   display: flex;
-`
+`;
 
 const LinkIcon = styled.a`
   margin-right: 10px;
-`
+`;
 
 interface IconProps {
-  width: string
-  height: string
+  width: string;
+  height: string;
 }
 
 const Icon = styled.img`
   width: ${(props: IconProps) => props.width};
   height: ${(props: IconProps) => props.height};
   margin-top: -4px;
-`
+`;
 
 const Container = styled.div`
   display: flex;
@@ -126,72 +127,70 @@ const Container = styled.div`
   @media (min-width: 1920px) {
     max-width: 1440px;
   }
-`
+`;
 
-const Footer: FC<FooterProps> = ({ }) => {
+const Footer: FC<FooterProps> = ({}) => {
+  //   const contact = [
+  //     {
+  //       icon: {
+  //         url: mapPin,
+  //         alt: "map",
+  //       },
+  //       text: data?.address,
+  //     },
+  //     {
+  //       icon: {
+  //         url: mobile,
+  //         alt: "phone",
+  //       },
+  //       text: data?.phone,
+  //     },
+  //     {
+  //       icon: {
+  //         url: globe,
+  //         alt: "web",
+  //       },
+  //       text: data?.website,
+  //     },
+  //   ]
 
-//   const contact = [
-//     {
-//       icon: {
-//         url: mapPin,
-//         alt: "map",
-//       },
-//       text: data?.address,
-//     },
-//     {
-//       icon: {
-//         url: mobile,
-//         alt: "phone",
-//       },
-//       text: data?.phone,
-//     },
-//     {
-//       icon: {
-//         url: globe,
-//         alt: "web",
-//       },
-//       text: data?.website,
-//     },
-//   ]
+  //   const icons = [
+  //     {
+  //       url: data?.instagram,
+  //       icon: {
+  //         url: ins,
+  //         alt: "ins",
+  //       },
+  //     },
+  //     {
+  //       url: data?.facebook,
+  //       icon: {
+  //         url: fb,
+  //         alt: "fb",
+  //       },
+  //     },
+  //     {
+  //       url: data?.youtube,
+  //       icon: {
+  //         url: yout,
+  //         alt: "youtube",
+  //       },
+  //     },
+  //   ]
 
-//   const icons = [
-//     {
-//       url: data?.instagram,
-//       icon: {
-//         url: ins,
-//         alt: "ins",
-//       },
-//     },
-//     {
-//       url: data?.facebook,
-//       icon: {
-//         url: fb,
-//         alt: "fb",
-//       },
-//     },
-//     {
-//       url: data?.youtube,
-//       icon: {
-//         url: yout,
-//         alt: "youtube",
-//       },
-//     },
-//   ]
-
-useEffect(() => {
-    const script = document.createElement("script")
+  useEffect(() => {
+    const script = document.createElement("script");
     script.src =
-      "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0"
-    script.async = true
-    script.defer = true
+      "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0";
+    script.async = true;
+    script.defer = true;
 
-    document.body.appendChild(script)
-
-  }, [])
-
+    document.body.appendChild(script);
+  }, []);
 
   return (
     <Wrapper>
+      <Chatboxes></Chatboxes>
       <Container>
         <Left>
           <Name>Halkeeping</Name>
@@ -210,7 +209,10 @@ useEffect(() => {
             data-hide-cover="false"
             data-show-facepile="true"
           >
-            <blockquote cite="https://www.facebook.com/halkeepingdanang/" className="fb-xfbml-parse-ignore">
+            <blockquote
+              cite="https://www.facebook.com/halkeepingdanang/"
+              className="fb-xfbml-parse-ignore"
+            >
               <a href="https://www.facebook.com/halkeepingdanang/">Facebook</a>
             </blockquote>
           </FBPage>
@@ -218,33 +220,27 @@ useEffect(() => {
         <Center>
           <Contact>Thông tin Halkeeping</Contact>
           <ContactWrapper>
-            
-              <ContactItem>
+            <ContactItem>
               Địa chỉ: 86 Phan Phu Tiên, Thanh Khê, Đà Nẵng
-                
-                {/* <ContactText>{text}</ContactText> */}
-              </ContactItem>
-              <ContactItem>
-              Số điện thoai / zalo: 038 993 6574 - 070 6145980
-                
-                {/* <ContactText>{text}</ContactText> */}
-              </ContactItem>
-              <ContactItem>
+              {/* <ContactText>{text}</ContactText> */}
+            </ContactItem>
+            <ContactItem>
+              Số điện thoai / zalo: 0706145980
+              {/* <ContactText>{text}</ContactText> */}
+            </ContactItem>
+            <ContactItem>
               Facebook: Dịch vụ dọn vệ sinh theo giờ Đà Nẵng - HalKeeping
-                </ContactItem>
+            </ContactItem>
           </ContactWrapper>
         </Center>
         <Right>
-        <Contact>Liên hệ chúng tôi</Contact>
-            <ContactItem>Email: halStorm13@gmail.com
-            </ContactItem>
-            <ContactItem>website: https://halkeeping.web.app/
-            </ContactItem>
-            
+          <Contact>Liên hệ chúng tôi</Contact>
+          <ContactItem>Email: halStorm13@gmail.com</ContactItem>
+          <ContactItem>website: https://halkeeping.web.app/</ContactItem>
         </Right>
       </Container>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
